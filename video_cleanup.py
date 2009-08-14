b4589e66300478ca5054ad368541a388d7c8ea3f
Justin Chouinard <justin@chouinardconsulting.com>
2009-08-14 17:46:20 -0400

    use os.walk instead of relying on shell

--- video_cleanup.py
+++ video_cleanup.py
@@ -21,6 +21,7 @@
 from subprocess import Popen, PIPE
 import sys
 import re
+import os
 
 minimum = {
     'xres': 320,
@@ -33,11 +34,12 @@ ALLOWED_FILES = '\.(asf|wmv|mpg|avi|mpeg|mov|mp4)$';
 allowedFileRe = re.compile(ALLOWED_FILES)
 vidRe = re.compile('^VIDEO:\s+\[\w+\]\s+(\d+)x(\d+)\s+(\d+)bpp\s+([\d\.]+)\s+fps\s+([\d\.]+)\s+kbps', re.MULTILINE)
 
-for fileName in sys.argv[1:]:
+def check_file(fileName):
+    print fileName
+    return
     if allowedFileRe.search(fileName) == None:
-        #print "File %s does not match the ALLOWED_FILES regex" % (fileName)
-        continue
-   
+        return
+    
     output = Popen(("mplayer", "-identify", "-frames", "0", "-ao", "null", fileName), stdout=PIPE, stderr=PIPE).communicate()[0]
     #VIDEO:  [XVID]  512x384  24bpp  29.970 fps  1196.3 kbps (146.0 kbyte/s)
     match = vidRe.search(output)
@@ -49,13 +51,29 @@ for fileName in sys.argv[1:]:
             'bpp': match.group(3),
             'fps': match.group(4),
             'kbps': abs(float(match.group(5)))
-            }
+        }
 
-        for key in minimum:
-            if videoInfo[key] == 0:
-                pass
-            elif videoInfo[key] < minimum[key]:
-                print '# %s at %d did not reach the minimum of %d' % (key, videoInfo[key], minimum[key])
-                print 'rm \"%s\"' % (fileName)
-                break
+    for key in minimum:
+        if videoInfo[key] == 0:
+            pass
+        elif videoInfo[key] < minimum[key]:
+            print '# %s at %d did not reach the minimum of %d' % (key, videoInfo[key], minimum[key])
+            print 'rm \"%s\"' % (fileName)
+            break
+
+def check_directory(path):
+    for root, dirs, files in os.walk(path):
+        for directory in dirs:
+            check_directory(directory)
+        for fileName in files:
+            check_file(os.path.join(root, fileName))
         
+
+def main():
+    for path in sys.argv[1:]:
+        check_directory(path)
+
+if __name__ == '__main__':
+    main()
+
+
