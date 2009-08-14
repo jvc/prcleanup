e6e7b9537191f8ff1da1ec39d05f7fbf8e6f9cbe
Justin Chouinard <justin@chouinardconsulting.com>
2009-08-14 17:39:59 -0400

    usage

---
+++ video_cleanup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#
+# video_cleanup.py - Delete poor quality videos.
+#
+# Author: Justin Chouinard <jchouinard@blackpantssoftware.com>
+#
+# Possibly the most handy hack-job script ever written. Instead of removing the
+# files, I prefer to print output that can be piped to /bin/sh. That way I can
+# look it over first, then run sh on it to actually remove.
+#
+# Example usage:
+#
+# 1.) Build the script
+#  % ./video_cleanup.py **/*.* > rm_script
+# 2.) Examine
+#  % cat rm_script
+# 3.) Execute
+#  % cat rm_script | sh
+#
+
+from subprocess import Popen, PIPE
+import sys
+import re
+
+minimum = {
+    'xres': 320,
+    'yres': 240,
+    'kbps': 400
+    }
+
+ALLOWED_FILES = '\.(asf|wmv|mpg|avi|mpeg|mov|mp4)$';
+
+allowedFileRe = re.compile(ALLOWED_FILES)
+vidRe = re.compile('^VIDEO:\s+\[\w+\]\s+(\d+)x(\d+)\s+(\d+)bpp\s+([\d\.]+)\s+fps\s+([\d\.]+)\s+kbps', re.MULTILINE)
+
+for fileName in sys.argv[1:]:
+    if allowedFileRe.search(fileName) == None:
+        #print "File %s does not match the ALLOWED_FILES regex" % (fileName)
+        continue
+   
+    output = Popen(("mplayer", "-identify", "-frames", "0", "-ao", "null", fileName), stdout=PIPE, stderr=PIPE).communicate()[0]
+    #VIDEO:  [XVID]  512x384  24bpp  29.970 fps  1196.3 kbps (146.0 kbyte/s)
+    match = vidRe.search(output)
+    if match != None:
+        videoInfo = {
+            'fileName': fileName,
+            'xres': int(match.group(1)),
+            'yres': int(match.group(2)),
+            'bpp': match.group(3),
+            'fps': match.group(4),
+            'kbps': abs(float(match.group(5)))
+            }
+
+        for key in minimum:
+            if videoInfo[key] == 0:
+                pass
+            elif videoInfo[key] < minimum[key]:
+                print '# %s at %d did not reach the minimum of %d' % (key, videoInfo[key], minimum[key])
+                print 'rm \"%s\"' % (fileName)
+                break
+        
